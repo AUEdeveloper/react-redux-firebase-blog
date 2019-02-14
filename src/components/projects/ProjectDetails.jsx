d60ec21109bc306abc5a29c053436d30add6bb31
yan <bryansky.yan2012@yandex.ru>
2019-02-14 10:29:37 +0300

    customized and exported projectdetails component to app component

---
+++ src/components/projects/ProjectDetails.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ProjectDetails = (props) => {
+  const id = props.match.params.id;
+  return (
+    <div className="container section project-details">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Project Title - {id}</span>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo dolores obcaecati magni deserunt eius eveniet sed, provident, eaque velit est sint repellat earum sapiente quae dolorum distinctio corrupti delectus architecto.</p>
+        </div>
+        <div className="card-action gret lighten-4 grey-text">
+          <div>Posted by Yan Bryansky</div>
+          <div>14 February, 10am</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectDetails
