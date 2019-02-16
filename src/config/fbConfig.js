407acab833bdb20815eaa3560dbe5e12317f2095
yan <bryansky.yan2012@yandex.ru>
2019-02-16 19:38:30 +0300

    added redux-thunk function and connected with firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+//initialize Firebase
+var config = {
+  apiKey: "AIzaSyCcIH0qS1KMLMBuhIZaHPhdTFkwUai-VMg",
+  authDomain: "my-react-redux-blog.firebaseapp.com",
+  databaseURL: "https://my-react-redux-blog.firebaseio.com",
+  projectId: "my-react-redux-blog",
+  storageBucket: "my-react-redux-blog.appspot.com",
+  messagingSenderId: "496371132614"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,12 +1,16 @@
 const initState = {
   projects: [
     {id: '1', title: 'first title', content: 'blah blah blah'},
-    {id: '2', title: 'secondt title', content: 'blah blah blah'},
+    {id: '2', title: 'second title', content: 'blah blah blah'},
     {id: '3', title: 'third title', content: 'blah blah blah'}
   ]
 }
 
 const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    case 'CREATE_PROJECT':
+      console.log('Created new project!', action.project);
+  }
   return state;
 }
 
