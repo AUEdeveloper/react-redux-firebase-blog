86f3601c66784c47d6d9bd1bb9fdd87a2836027e
yan <bryansky.yan2012@yandex.ru>
2019-02-18 12:47:40 +0300

    made route guarding

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,13 +4,15 @@ import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
 import { connect } from 'react-redux';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { auth } = props;
+  //console.log(auth);
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
   return (
     <nav className="nav-wrapper deep-purple darken-1">
       <div className="container">
         <Link to='/' className='brand-logo left'>MyReactBlog</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        { links }
       </div>
     </nav>
   );
@@ -19,7 +21,7 @@ const Navbar = () => {
 const mapStateToProps = (state) => {
   //console.log(state);
   return {
-    
+    auth: state.firebase.auth
   }
 }
 
